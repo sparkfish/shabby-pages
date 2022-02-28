78fbdc667604cae258f2c82ae4d40ec929c87179
alex <source@proof.construction>
2022-02-28 15:44:44 -0600

    fix comments in shabbypipeline

--- build/shabbypipeline.py
+++ build/shabbypipeline.py
@@ -5,114 +5,113 @@ import cv2
 
 
 ################################################################################
-# EXPRESSING VARIATION
+# CONTROLLING VARIATION
 #
 # We can control the outputs of the pipeline by setting values here.
 ################################################################################
 
-# Dithering.dither can be 'ordered' for ordered dithering or another string for floyd-steinberg dithering
-# Dithering.order determines the dimensions of the threshold map
+# Dithering.dither_type can be 'ordered' for ordered dithering or another string for floyd-steinberg dithering
 dithering_dither_type = random.choice(["ordered", "floyd-steinberg"])
+# Dithering.order determines the dimensions of the threshold map
 dithering_order = random.choice(range(1,10))
 
 # InkBleed.intensity_range is a tuple with bounds for bleed intensity to be selected from
-# InkBleed.color_range is a tuple with bounds for color noise
-# InkBleed.kernel_size determines the radius of the bleed effect
-# InkBleed.severity determines significance of bleed effect
 inkbleed_intensity_range = (0.1, 0.2)
+# InkBleed.color_range is a tuple with bounds for color noise
 inkbleed_color_range = (0, 224)
+# InkBleed.kernel_size determines the radius of the bleed effect
 inkbleed_kernel_size = (5, 5)
+# InkBleed.severity determines significance of bleed effect
 inkbleed_severity=(0.4, 0.6)
 
 # BleedThrough.intensity_range is a tuple with bounds for bleed intensity to be selected from
-# BleedThrough.color_range is a tuple with bounds for color noise
-# BleedThrough.ksize is a tuple of height/width pairs for sampling kernel size
-# BleedThrough.sigmaX is the standard deviation of the kernel along the x-axis
-# BleedThrough.alpha is the intensity of the bleeding effect, recommended 0.1-0.5
-# BleedThrough.offsets is a pair of x and y distances to shift the bleed with
 bleedthrough_intensity_range=(0.1, 0.2)
+# BleedThrough.color_range is a tuple with bounds for color noise
 bleedthrough_color_range=(0, 224)
+# BleedThrough.ksize is a tuple of height/width pairs for sampling kernel size
 bleedthrough_ksize=(17, 17)
+# BleedThrough.sigmaX is the standard deviation of the kernel along the x-axis
 bleedthrough_sigmaX=0
+# BleedThrough.alpha is the intensity of the bleeding effect, recommended 0.1-0.5
 bleedthrough_alpha=0.3
+# BleedThrough.offsets is a pair of x and y distances to shift the bleed with
 bleedthrough_offsets=(10, 20)
 
 # Letterpress.n_samples is a tuple determining how many points to generate per cluster
-# Letterpress.n_clusters is a tuple determining how many clusters to generate
-# Letterpress.std_range is a pair of ints determining the std deviation range in each blob
-# Letterpress.value_range determines values that points in the blob are sampled from
-# Letterpress.value_threshold_range is the minimum pixel value to enable the effect (e.g. 128)
-# Letterpress.blur enables blur in the noise mask
 letterpress_n_samples=(100, 200)
+# Letterpress.n_clusters is a tuple determining how many clusters to generate
 letterpress_n_clusters=(500, 1000)
+# Letterpress.std_range is a pair of ints determining the std deviation range in each blob
 letterpress_std_range=(500, 500)
+# Letterpress.value_range determines values that points in the blob are sampled from
 letterpress_value_range=(200, 255)
+# Letterpress.value_threshold_range is the minimum pixel value to enable the effect (e.g. 128)
 letterpress_value_threshold_range=(128, 128)
+# Letterpress.blur enables blur in the noise mask
 letterpress_blur=1
 
-# LowInkRandomLines.count_range is a pair determining how many lines should be drawn
-# LowInkRandomLines.use_consistent_lines is false if we should vary the width and alpha of lines
-# LowInkPeriodicLines.period_range is a pair determining how wide the gap between lines can be
+# LowInkLines.count_range is a pair determining how many lines should be drawn
 lowinkrandomlines_count_range = (5, 10)
-lowinkrandomlines_use_consistent_lines=random.choice([True, False])
 lowinkperiodiclines_count_range = (2, 5)
-lowinkperiodiclines_period_range=(10, 30)
+# LowInkLines.use_consistent_lines is false if we should vary the width and alpha of lines
+lowinkrandomlines_use_consistent_lines=random.choice([True, False])
 lowinkperiodiclines_use_consistent_lines=random.choice([True, False])
+# LowInkPeriodicLines.period_range is a pair determining how wide the gap between lines can be
+lowinkperiodiclines_period_range=(10, 30)
 
 # PaperFactory.tile_texture_shape determines the range from which to sample texture dimensions
-# PaperFactory.texture_path is the directory to pull textures from
 paperfactory_tile_texture_shape = (250, 250)
+# PaperFactory.texture_path is the directory to pull textures from
 paperfactory_texture_path = "./paper_textures"
 
 # NoiseTexturize.sigma_range defines bounds of noise fluctuations
-# NoiseTexturize.turbulence_range defines how quickly big patterns are replaced with small ones; lower means more iterations
 noisetexturize_sigma_range = (3, 10)
+# NoiseTexturize.turbulence_range defines how quickly big patterns are replaced with small ones; lower means more iterations
 noisetexturize_turbulence_range = (2, 5)
 
 # BrightnessTexturize.range determines the value range of samples for the brightness matrix
-# BrightnessTexturize.deviation is additional variation for the uniform sample
 brightnesstexturize_range = (0.9, 0.99)
+# BrightnessTexturize.deviation is additional variation for the uniform sample
 brightnesstexturize_deviation = 0.03
 
 # Brightness.range is a pair of floats determining the brightness delta
 brightness_range = (0.8, 1.4)
 
 # PageBorder.side determines the page edge of the effect
-# PageBorder.width_range determines border thickness
-# PageBorder.pages determines how many page shadows to render
 pageborder_side = random.choice(["left", "top", "bottom", "right"])
+# PageBorder.width_range determines border thickness
 pageborder_width_range = (5, 30)
+# PageBorder.pages determines how many page shadows to render
 pageborder_pages = None # internally this is random.randint(2, 7)
 
 # DirtyRollers.line_width_range determines the width of roller lines
-# DirtyRollers.scanline_type changes the background of lines
 dirtyrollers_line_width_range = (8, 12)
+# DirtyRollers.scanline_type changes the background of lines
 dirtyrollers_scanline_type = 0
 
 # LightingGradient.mask_size determines how big the mask should be
-# LightingGradient.position defines the center of the light strip
-# LightingGradient.direction indicates the rotation degree of the light strip
-# LightingGradient.max_brightness and LightingGradient.min_brightness set bounds for how much brightness change will happen
-# LightingGradient.mode is linear or gaussian depending on how light should decay
-# LightingGradient.linear_decay_rate is only valid in linear static mode
-# LightingGradient.transparency gives the transparency of the input image
 lightinggradient_light_position = None
+# LightingGradient.direction indicates the rotation degree of the light strip
 lightinggradient_direction = None
+# LightingGradient.max_brightness and LightingGradient.min_brightness set bounds for how much brightness change will happen
 lightinggradient_max_brightness = 255
 lightinggradient_min_brightness = 0
+# LightingGradient.mode is linear or gaussian depending on how light should decay
 lightinggradient_mode = random.choice(["linear_dynamic", "linear_static", "gaussian"])
+# LightingGradient.linear_decay_rate is only valid in linear static mode
 lightinggradient_linear_decay_rate = None
+# LightingGradient.transparency gives the transparency of the input image
 lightinggradient_transparency = None
 
 # DirtyDrum.line_width_range determines the range from which drum line widths are sampled
-# DirtyDrum.direction is 0 for horizontal, 1 for vertical, 2 for both
-# DirtyDrum.noise_intensity changes how significant the effect is, recommended 0.8-1.0
-# DirtyDrum.ksize is a tuple of height/width pairs from which to sample kernel size
-# DirtyDrum.sigmaX is the stdev of the kernel in the x direction
 dirtydrum_line_width_range = (2, 8)
+# DirtyDrum.direction is 0 for horizontal, 1 for vertical, 2 for both
 dirtydrum_direction = random.randint(0,2)
+# DirtyDrum.noise_intensity changes how significant the effect is, recommended 0.8-1.0
 dirtydrum_noise_intensity = 0.95
+# DirtyDrum.ksize is a tuple of height/width pairs from which to sample kernel size
 dirtydrum_ksize = (3, 3)
+# DirtyDrum.sigmaX is the stdev of the kernel in the x direction
 dirtydrum_sigmaX = 0
 
 # SubleNoise.range gives the variation range for sampling noise
@@ -122,12 +121,6 @@ subtlenoise_range = 10
 jpeg_quality_range = (25, 95)
 
 # BindingsAndFasteners.overlay_types can be min, max, or mix
-# BindingsAndFasteners.foreground is the path to fg image or the image itself
-# BindingsAndFasteners.effect_type is "punch_holes", "binding_holes", or "clips"
-# BindingsAndFasteners.ntimes gives how many fg images to draw
-# BindingsAndFasteners.nscales is the scale of the fg image size
-# BindingsAndFasteners.edge gives the edge to place the images on
-# BindingsAndFasteners.edge_offset is how far from the page edge to draw
 bindingsandfasteners_overlay_types = random.choice([
     "min", "max", "mix", "normal",
     "lighten", "darken", "addition", "subtract",
@@ -135,27 +128,33 @@ bindingsandfasteners_overlay_types = random.choice([
     "divide", "hard_light", "grain_extract",
     "grain_merge", "overlay"
 ])
+# BindingsAndFasteners.foreground is the path to fg image or the image itself
 bindingsandfasteners_foreground = None
+# BindingsAndFasteners.effect_type is "punch_holes", "binding_holes", or "clips"
 bindingsandfasteners_effect_type = random.choice(["punch_holes", "binding_holes", "clips"])
+# BindingsAndFasteners.ntimes gives how many fg images to draw
 bindingsandfasteners_ntimes = 3
+# BindingsAndFasteners.nscales is the scale of the fg image size
 bindingsandfasteners_nscales = (1,1)
+# BindingsAndFasteners.edge gives the edge to place the images on
 bindingsandfasteners_edge = random.choice(["left", "top", "bottom", "right"])
+# BindingsAndFasteners.edge_offset is how far from the page edge to draw
 bindingsandfasteners_edge_offset = 50
 
 # Gamma.range is an interval from which to sample a gamma shift
 gamma_range = (0.5, 1.5)
 
 # Geometric.scale is a pair determining how to scale the image
-# Geometric.translation is a pair determining where to translate the image
-# Geometric.fliplr flips the image left and right
-# Geometric.flipud flips the image up and down
-# Geometric.crop four points giving the corners of a crop region
-# Geometric.rotate_range a pair determining the rotation angle sample range
 geometric_scale = (1, 1)
+# Geometric.translation is a pair determining where to translate the image
 geometric_translation = (0, 0)
+# Geometric.fliplr flips the image left and right
 geometric_fliplr = random.choice([0,1])
+# Geometric.flipud flips the image up and down
 geometric_flipud = random.choice([0,1])
+# Geometric.crop four points giving the corners of a crop region
 geometric_crop = ()
+# Geometric.rotate_range a pair determining the rotation angle sample range
 geometric_rotate_range = (0, 0)
 
 
