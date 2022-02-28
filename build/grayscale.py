adac73394bb98cb107d0050a2c86ec3300bc432d
alex <source@proof.construction>
2022-02-28 15:44:18 -0600

    output all grayscale images to a separate directory

--- build/grayscale.py
+++ build/grayscale.py
@@ -6,7 +6,7 @@ def make_grayscale(filename):
     if "png" in filename:
         img = cv2.imread(filename)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        save_name = f[:-4] + "-gray.png"
+        save_name = "grayscale/" + f[:-4] + "-gray.png"
         cv2.imwrite(save_name, gray)
 
 if __name__ = "__main__":
